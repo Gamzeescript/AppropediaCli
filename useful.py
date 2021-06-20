fb58758a9b983540305aff6f0b134edc63e1f6bc
Mihaeru <gamzeescript@outlook.com>
2021-06-19 19:26:52 -0600

    primme commit
    aqui subiendo el proyecto

---
+++ useful.py
@@ -0,0 +1,60 @@
+        #authors
+        #pattern4 = re.compile('')
+        #status
+        #pattern5 = re.compile('')
+        #made
+        #pattern6 = re.compile('')
+        #replicated
+        #pattern6 =  re.compile('')
+        #made-in
+        #pattern11 = re.compile('')
+        #designed-in
+        #pattern12 = re.compile('')
+        #replicated-in
+        #pattern13 = re.compile('')
+        #affiliations
+        #pattern14 = re.compile('')
+        #materials
+        #pattern15 = re.compile('')
+        #cost
+        #pattern16 = re.compile('')
+        #cost-currency
+        #pattern17 = re.compile('')
+        #derivative-of 
+        #pattern19 = re.compile('')
+        #translators
+        #pattern20 = re.compile('')
+        #license-hardware
+        #pattern21 = re.compile('')
+        #license-documentation
+        #pattern22 = re.compile('')
+        #license-software
+        #pattern23 = re.compile('')
+        #licensor
+        #pattern24 = re.compile('')
+        #making-instructions 
+        #pattern25 = re.compile('')
+        #manufacturing-files
+        #pattern26 = re.compile('')
+        #risk-assessment
+        #pattern27 = re.compile('')
+        #tool-settings
+        #pattern28 = re.compile('')
+        #quality-instructions
+        #pattern29 = re.compile('')
+        #operating-instructions
+        #pattern30 = re.compile('')
+        #maintenance-instructions
+        #pattern31 = re.compile('')
+        #disposal-instructions
+        #pattern32 = re.compile('')
+        #design-files 
+        #pattern33 = re.compile('')
+        #schematics
+        #pattern34 = re.compile('')
+        #bom
+        #pattern35 = re.compile('')
+        #tools
+        #pattern36 = re.compile('')
+        #software
+        #pattern37 = re.compile('')
