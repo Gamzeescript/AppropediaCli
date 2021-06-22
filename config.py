c7f882691209a53f54c890adcb1ca2d556daca96
Mihaeru <gamzeescript@outlook.com>
2021-06-22 13:31:27 -0600

    more
    configuradciones para poder deployar en linea

---
+++ config.py
@@ -0,0 +1,17 @@
+from decouple import config
+
+class Config:
+    pass
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+class ProductionConfig(Config):
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = config('DATABASE_URL', default='localhost')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+config = {
+    'development': DevelopmentConfig,
+    'production': ProductionConfig
+}
